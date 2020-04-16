d0ba154324bbbdc1ced55801279cde35af2893b6
egonio <eldon.gonio@gmail.com>
2020-04-15 23:54:15 -0400

    Add project files.

---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,46 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+        public void Add(Restaurant restaurant)
+        {
+            // it is not added yet, this is just unit of work
+            // kinda like a transaction
+            db.Restaurants.Add(restaurant);
+            // next step saves it
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in db.Restaurants
+                   orderby r.Name
+                   select r;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var entry = db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer(HttpConfiguration httpConfiguration)
+        {
+            var builder = new ContainerBuilder();
+
+            // first register controllers - it will get all the controllers
+            // MvcApplication is the assymbly in Global.asax
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // For register API controllers
+            builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
+
+            // Registed the type InMemoryRestaurantData whenever someone ask for IRestaurantData
+            builder.RegisterType<SqlRestaurantData>()
+                .As<IRestaurantData>()
+                .InstancePerRequest(); // need instance per request not singleton
+            builder.RegisterType<OdeToFoodDbContext>().InstancePerRequest();
+            
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
